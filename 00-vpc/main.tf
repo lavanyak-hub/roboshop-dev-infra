57fe01944a10f20f99b23358ef482339958d9509
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-12-09 19:20:22 +0530

    Remove provider files

---
+++ 00-vpc/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source = "git::https://github.com/lavanyak-hub/terraform-aws-vpc.git"
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    public_subnet_cidrs = var.public_subnet_cidrs
+
+    private_subnet_cidrs = var.private_subnet_cidrs
+
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
+}
+
