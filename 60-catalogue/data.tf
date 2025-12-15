e0ed03ba35fc13b285e137df14039e1a5cac62ac
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-12-15 18:41:44 +0530

    some message

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -22,10 +22,6 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
-}
-
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
 }
