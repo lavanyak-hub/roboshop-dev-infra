0038ff141add6ab969abb59f120e2f96317245bf
lavanyak-hub <lavanyakotha1999@gmail.com>
2026-01-07 18:11:05 +0530

    some message

---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+
+variable "zone_id" {
+  default = "Z04936233JDYCWRJS57S7"
+}
+
+variable "domain_name" {
+  default = "rakesh12.fun"
+}
